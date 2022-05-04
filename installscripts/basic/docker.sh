bba7526e30176e34b96b498b72ae7c1ea6a0c9f9
Carles Llobet <krow.boarder.9@gmail.com>
2022-05-04 12:32:25 +0200

    Adding docker buildx to docker installation

--- installscripts/basic/docker.sh
+++ installscripts/basic/docker.sh
@@ -19,4 +19,16 @@ DESTINATION=/usr/local/bin/docker-compose
 sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
 sudo chmod 755 $DESTINATION
 
+# Download docker buildx
+VERSION=$(curl --silent https://api.github.com/repos/docker/buildx/releases/latest | grep -Po '"tag_name": "\K.*\d')
+DESTINATION=~/.docker/cli-plugins/docker-buildx
+
+mkdir -p ~/.docker/cli-plugins
+ARCH=$(uname -m)
+if [ $ARCH == "aarch64" ] 
+	then 
+		ARCH="arm64";
+fi
+curl -L https://github.com/docker/buildx/releases/download/${VERSION}/buildx-${VERSION}.$(uname -s)-$ARCH -o $DESTINATION
+
 return 0
